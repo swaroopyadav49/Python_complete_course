f5c7f583f04dc4333c05f29f81123515209cc1e9
Swaroop kumar <68234285+swaroopyadav49@users.noreply.github.com>
2024-10-16 17:27:25 +0530

    File_Attached

---
+++ Logging/logs/logger.py
@@ -0,0 +1,10 @@
+import logging
+
+## configure the basic logging setting.
+logging.basicConfig(
+    filename='app.log',
+    filemode='w',
+    level=logging.DEBUG,
+    format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+    )
